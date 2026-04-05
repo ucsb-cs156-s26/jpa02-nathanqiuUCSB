db4612f3fe16937662babe777a9479a33fe5e40a
Nathan Qiu <nathanqiu@ucsb.edu>
2026-04-05 16:06:22 -0700

    devloper fixes

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -28,5 +28,23 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+    public void getGithubId_returns_correct_githubId(){
+        assertEquals("nathanqiuUCSB", Developer.getGithubId());
+    }
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("s26-09", t.getName());
+    }
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Clement"),"Team should contain Clement");
+        assertTrue(t.getMembers().contains("Cooper"),"Team should contain Cooper");
+        assertTrue(t.getMembers().contains("Nathan Q"),"Team should contain Nathan Q");
+        assertTrue(t.getMembers().contains("Parth"),"Team should contain Parth");
+        assertTrue(t.getMembers().contains("Ritam"),"Team should contain Ritam");
+        assertTrue(t.getMembers().contains("Ryan"),"Team should contain Ryan");
+    }
 }
